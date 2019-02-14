ab0cf43a9fe0ba9e27d9724bef90e5b9036d73ef
Adithya280399 <47312447+Adithya280399@users.noreply.github.com>
2019-02-14 08:54:39 +0530

    Update LargestNumber.java

--- LargestNumber.java
+++ LargestNumber.java
@@ -2,17 +2,17 @@ import java.util.Scanner;
 class LargestNumber{
 public static void main(String[] args){
 System.out.println("Enter the number");
-Scanner n=new Scanner(System.in);
-int x=scan.nextint();
-int y =scan.nextint();
-int z=scan.nextint();
-if(x>>y&&x>>z){
+Scanner scan=new Scanner(System.in);
+int x=scan.nextInt();
+int y=scan.nextInt();
+int z=scan.nextInt();
+if(x>y&&x>z){
 System.out.println(x+"is largest");
 }
-else if(y>>x&&y>>z){
+else if(y>x&&y>z){
 System.out.println(y+"is that largest");
 }
-else if(z>>x&&z>>y){
+else {
 System.out.println(z+"is the largest");
 }
 }
