a3040d8e9a7afa2145088e0a5db372e6726760a8
Adithya280399 <47312447+Adithya280399@users.noreply.github.com>
2019-02-14 09:18:19 +0530

    Update Leap year.java

--- Leap year.java
+++ Leap year.java
@@ -3,11 +3,17 @@ class Leapyear{
 public static void main(String[] qwe){
 Scanner scan=new Scanner(System.in); 
 int year=scan.nextint();
-If{year%400==0)||(year%4==0&&year%100!=0)
-System.out.println("Yes");
-}
-else{ 
-System.out.println("No");
-}
+if{year%4==0){
+  if{year%100==0){
+    if{year%400==0)
+System.out.println("yes");
+else
+System.out.println("no");
+      }
+       else
+       System.out.println("yes");
+  }
+     else
+       System.out.println("no");
 }
 }
