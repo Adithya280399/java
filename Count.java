274bb6cbb5ce9b113ef06a4e5fa1010a86b3a353
Adithya280399 <47312447+Adithya280399@users.noreply.github.com>
2019-02-14 09:45:40 +0530

    Update Count.java

--- Count.java
+++ Count.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 class Count{
 public static void main(String[] args){
-System.out.println("enter the numberof times");
 Scanner n=new Scanner(System.in);
 int a=n.nextInt();
 int count=0;
@@ -9,6 +8,6 @@ while(a>0){
 a=a/10;
 count++;
 }
-System.out.println("the number of digitts in the number is"+count);
+System.out.println(count);
 }
 }
