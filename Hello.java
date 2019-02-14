7c5031171673b77429cb8ab9558442324a49f251
Adithya280399 <47312447+Adithya280399@users.noreply.github.com>
2019-02-14 09:23:34 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 class Hello{
 public static void main(String[] args){
-System.out.println("enter the numberof times");
 Scanner n=new Scanner(System.in);
 int a=n.nextInt();
 while(a>0){
